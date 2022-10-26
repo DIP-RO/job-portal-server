312deb96a0e4c3b48317ba80c25ecb3e390e01a0
Sohag <sohaghossain7th@gmail.com>
2022-10-26 18:15:26 +0600

    Single course find by corse/dynamic route

--- index.js
+++ index.js
@@ -10,14 +10,22 @@ app.use(cors());
 
 const courses = require('./Data/courses.json');
 
-
+// get all courses 
 app.get('/courses', (req, res) => {
     res.send(courses);
+})
+// geet single course by course_id 
+app.get('/course/:course_id', (req, res) => {
+    const course_id = parseInt(req.params.course_id);
+    const course = courses.find(course => course.id === course_id)
+    res.send(course);
 })
 
 app.get('/', (req, res) => {
     res.send("Fun Code server is running");
 })
+
+
 
 app.listen(port, () => {
  console.log(`Fun Code Running On Port ${port}`);
